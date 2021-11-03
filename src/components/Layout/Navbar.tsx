9a573c07159b015e3a8c96d986f77ae41662471a
funmilola <funmilolajire@gmail.com>
2021-11-04 00:37:34 +0200

    layout

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             <div className={styles.image}>
                 <Image src="/assets/shared/logo.svg" className={styles.logo} layout='fill' alt="logo" />
             </div>
-            <nav>
+            <nav className={styles.large}>
                 <Link href="/" passHref><h5 className={router.asPath == "/" ? styles.active : ''}><span className={styles.number}>00</span>Home</h5></Link>
                 <Link href="/destination" passHref><h5 className={router.asPath == "/destination" ? styles.active : ''}><span className={styles.number}>01</span>Destination</h5></Link>
                 <Link href="/crew" passHref><h5 className={router.asPath == "/crew" ? styles.active : ''}><span className={styles.number}>02</span>Crew</h5></Link>
